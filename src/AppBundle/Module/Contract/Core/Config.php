b7068f24ab8b29a5beab1309596a46d02d4ae60d
Clivern <hello@clivern.com>
2017-09-15 14:58:58 +0200

    Config service and contract added

---
+++ src/AppBundle/Module/Contract/Core/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Module\Contract\Core;
+
+/**
+ * Config Service Contract
+ *
+ * @package AppBundle\Module\Contract\Core
+ */
+interface Config
+{
+    #
+}
---
+++ src/AppBundle/Module/Service/Core/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Module\Service\Core;
+
+use AppBundle\Module\Contract\Core\Config as ConfigContract;
+
+/**
+ * Config Service
+ *
+ * @package AppBundle\Module\Service\Core
+ */
+class Config implements ConfigContract
+{
+	#
+}
