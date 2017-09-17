b7a711067836f0cbb3944fe2164431f6b23a85b8
Clivern <hello@clivern.com>
2017-09-17 21:27:59 +0200

    travis will take api testing :)

--- tests/AppBundle/Controller/API/V1/ServerControllerTest.php
+++ tests/AppBundle/Controller/API/V1/ServerControllerTest.php
@@ -4,16 +4,21 @@ namespace Tests\AppBundle\Controller\API\V1;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class UserControllerTest extends WebTestCase
+/**
+ * Server API Test Cases
+ *
+ * @package Tests\AppBundle\Controller\API\V1
+ */
+class ServerControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
         $client->request(
-		    'DELETE',
-		    '/api/v1/server/6?api_token=$2y$10$7263UTiI9ExW1HL05QnOLefmRHJtYXoOjh.LFRqUmZ3pI2Q5O99MS'
-		);
+            'DELETE',
+            '/api/v1/server/6?api_token=$2y$10$7263UTiI9ExW1HL05QnOLefmRHJtYXoOjh.LFRqUmZ3pI2Q5O99MS'
+        );
         $this->assertEquals($client->getResponse()->getContent(), '{"success":false,"payload":[],"messages":[{"type":"error","message":"Invalid Request."}]}');
     }
 }
