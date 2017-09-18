de4dcfc7744b5d6e45a94566e2202dd0af091c77
Clivern <hello@clivern.com>
2017-09-18 17:56:33 +0200

    Issue fixed on base template

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,6 @@
 var lwt_app = lwt_app || {};
 
+
 lwt_app.layout = (function (window, document, $) {
 
     var base = {
@@ -20,6 +21,69 @@ lwt_app.layout = (function (window, document, $) {
 })(window, document, jQuery);
 
 
+lwt_app.login = (function (window, document, $) {
+
+    var base = {
+
+        el: {
+
+        },
+
+        init: function(){
+
+        }
+    };
+
+   return {
+        init: base.init
+    };
+
+})(window, document, jQuery);
+
+
+lwt_app.profile = (function (window, document, $) {
+
+    var base = {
+
+        el: {
+
+        },
+
+        init: function(){
+
+        }
+    };
+
+   return {
+        init: base.init
+    };
+
+})(window, document, jQuery);
+
+
+lwt_app.server = (function (window, document, $) {
+
+    var base = {
+
+        el: {
+
+        },
+
+        init: function(){
+
+        }
+    };
+
+   return {
+        init: base.init
+    };
+
+})(window, document, jQuery);
+
+
 jQuery(document).ready(function($){
     lwt_app.layout.init();
+    lwt_app.login.init();
+    lwt_app.profile.init();
+    lwt_app.server.init();
 });
