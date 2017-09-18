82df3c2c8122824ca362fdb30d061f3ec9b21fc1
Clivern <hello@clivern.com>
2017-09-18 22:57:58 +0200

    add login api test case

--- tests/AppBundle/Controller/API/V1/LoginControllerTest.php
+++ tests/AppBundle/Controller/API/V1/LoginControllerTest.php
@@ -11,8 +11,36 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class LoginControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+    /**
+     * Test Auth Success
+     *
+     * @return void
+     */
+    public function testAuthSuccess()
     {
-        #
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v1/auth',
+            ['username' => 'clivern', 'password' => 'clivern']
+        );
+        $this->assertContains('"success":true', $client->getResponse()->getContent());
+    }
+
+    /**
+     * Test Auth Failure
+     *
+     * @return void
+     */
+    public function testAuthFailure()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v1/auth',
+            ['username' => 'cli', 'password' => 'clivern']
+        );
+        $this->assertContains('"success":false', $client->getResponse()->getContent());
     }
 }
