443f86c5b83d931dfde6bbc25992bc96729b2b9d
Clivern <hello@clivern.com>
2017-09-19 21:35:50 +0200

    authentication tests added

--- tests/AppBundle/Controller/Web/Admin/DashboardControllerTest.php
+++ tests/AppBundle/Controller/Web/Admin/DashboardControllerTest.php
@@ -3,6 +3,9 @@
 namespace Tests\AppBundle\Controller\Web\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\BrowserKit\Cookie;
+use AppBundle\Entity\User as UserEntity;
 
 /**
  * Dashboard Controller Test
@@ -11,16 +14,49 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class DashboardControllerTest extends WebTestCase
 {
+    /**
+     * @var UserEntity
+     */
+    protected $entityManager;
 
     /**
-     * Test Dashboard Page
+     * Class Constructor
+     */
+    public function __construct()
+    {
+        $client = static::createClient();
+        $this->entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    /**
+     * Test Unauthenticated Access
      *
      * @return void
      */
-    public function testIndex()
+    public function testUnAuthenticated()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/admin/dashboard');
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test Authenticated Access
+     *
+     * @return void
+     */
+    public function testAuthenticated()
+    {
+        $client = static::createClient();
+        $session = $client->getContainer()->get('session');
+        $firewall = 'main';
+        $user = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['username' => 'clivern']);
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+        $crawler = $client->request('GET', '/admin/dashboard');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
--- tests/AppBundle/Controller/Web/Admin/ServerControllerTest.php
+++ tests/AppBundle/Controller/Web/Admin/ServerControllerTest.php
@@ -3,6 +3,10 @@
 namespace Tests\AppBundle\Controller\Web\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\BrowserKit\Cookie;
+use AppBundle\Entity\User as UserEntity;
+use AppBundle\Entity\Server as ServerEntity;
 
 /**
  * Server Controller Test
@@ -13,11 +17,50 @@ class ServerControllerTest extends WebTestCase
 {
 
     /**
+     * @var UserEntity
+     */
+    protected $entityManager;
+
+    /**
+     * Class Constructor
+     */
+    public function __construct()
+    {
+        $client = static::createClient();
+        $this->entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    /**
+     * Test Create Server
+     *
+     * @return void
+     */
+    public function testCreateServer()
+    {
+        $server = $this->entityManager->getRepository(ServerEntity::class)->findOneBy(['assetId' => '623528']);
+
+        if( $server ){
+            $this->entityManager->remove($server);
+            $this->entityManager->flush();
+        }
+
+        $user = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['username' => 'clivern']);
+
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v1/server?api_token=' . $user->getApiToken(),
+            ['name'=>'R234', 'brand'=>'Dell', 'asset_id'=>'623528', 'price'=>'200.35']
+        );
+        $this->assertContains('"success":true', $client->getResponse()->getContent());
+    }
+
+    /**
      * Test List Page
      *
      * @return void
      */
-    public function testList()
+    public function testUnAuthenticatedList()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/admin/servers');
@@ -29,7 +72,7 @@ class ServerControllerTest extends WebTestCase
      *
      * @return void
      */
-    public function testAdd()
+    public function testUnAuthenticatedAdd()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/admin/servers/add');
@@ -41,11 +84,91 @@ class ServerControllerTest extends WebTestCase
      *
      * @return void
      */
-    public function testView()
+    public function testUnAuthenticatedView()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/admin/servers/1/view');
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 
+    /**
+     * Test Authenticated Access
+     *
+     * @return void
+     */
+    public function testAuthenticatedList()
+    {
+        $client = static::createClient();
+        $session = $client->getContainer()->get('session');
+        $firewall = 'main';
+        $user = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['username' => 'clivern']);
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+        $crawler = $client->request('GET', '/admin/profile');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test Authenticated Access
+     *
+     * @return void
+     */
+    public function testAuthenticatedAdd()
+    {
+        $client = static::createClient();
+        $session = $client->getContainer()->get('session');
+        $firewall = 'main';
+        $user = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['username' => 'clivern']);
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+        $crawler = $client->request('GET', '/admin/servers/add');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test Authenticated Access
+     *
+     * @return void
+     */
+    public function testAuthenticatedView()
+    {
+        $server = $this->entityManager->getRepository(ServerEntity::class)->findOneBy(['assetId' => '623528']);
+
+        $client = static::createClient();
+        $session = $client->getContainer()->get('session');
+        $firewall = 'main';
+        $user = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['username' => 'clivern']);
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+        $crawler = $client->request('GET', '/admin/servers/' . $server->getId() .  '/view');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test Delete Server
+     *
+     * @return void
+     */
+    public function testDeleteServer()
+    {
+        $server = $this->entityManager->getRepository(ServerEntity::class)->findOneBy(['assetId' => '623528']);
+        $user = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['username' => 'clivern']);
+
+        $client = static::createClient();
+        $client->request(
+            'DELETE',
+            '/api/v1/server/' . $server->getId() . '?api_token=' . $user->getApiToken()
+        );
+
+        $this->assertContains('"success":true', $client->getResponse()->getContent());
+    }
 }
