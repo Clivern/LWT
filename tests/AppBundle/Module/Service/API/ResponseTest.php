49b4474a162dcbe7e96b6f93d1b33ce591c4370f
Clivern <hello@clivern.com>
2017-09-19 13:13:00 +0200

    response unit tests added

--- tests/AppBundle/Module/Service/API/ResponseTest.php
+++ tests/AppBundle/Module/Service/API/ResponseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle\Module\Service\API;
 
 use PHPUnit\Framework\TestCase;
+use AppBundle\Module\Service\API\Response as ResponseService;
 
 /**
  * Response Service Test
@@ -11,8 +12,33 @@ use PHPUnit\Framework\TestCase;
  */
 class ResponseTest extends TestCase
 {
-    public function testCase()
+
+    /**
+     * @var ResponseService
+     */
+    protected $response;
+
+    /**
+     * Class Constructor
+     */
+    public function __construct()
     {
-        #
+        $this->response = new ResponseService();
+    }
+
+    /**
+     * Test Response Builder
+     *
+     * @return void
+     */
+    public function testResponseBuilder()
+    {
+        $this->response->setStatus(true);
+        $this->response->setPayload(['id' => 3, 'name' => 'Test'], true);
+        $this->response->setMessage(['type' => 'error', 'message' => 'Test']);
+        $this->assertTrue($this->response->getStatus());
+        $this->assertEquals($this->response->getPayload(), ['id' => 3, 'name' => 'Test']);
+        $this->assertEquals($this->response->getMessages(), [['type' => 'error', 'message' => 'Test']]);
+        $this->assertEquals($this->response->getResponse(), ["success" => true, "payload" => ['id' => 3, 'name' => 'Test'],"messages" => [['type' => 'error', 'message' => 'Test']]]);
     }
 }
