d633280890849b22a355e5e9208144de15b1c470
Clivern <hello@clivern.com>
2017-09-19 12:23:32 +0200

    add test cases for server service

--- tests/AppBundle/Module/Service/Core/ServerTest.php
+++ tests/AppBundle/Module/Service/Core/ServerTest.php
@@ -2,17 +2,163 @@
 
 namespace Tests\AppBundle\Module\Service\Core;
 
-use PHPUnit\Framework\TestCase;
+use AppBundle\Module\Service\Core\User as UserService;
+use AppBundle\Module\Service\Core\Server as ServerService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
  * Server Service Test
  *
  * @package Tests\AppBundle\Module\Service\Core
  */
-class ServerTest extends TestCase
+class ServerTest extends KernelTestCase
 {
-    public function testCase()
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var UserService
+     */
+    protected $user;
+
+    /**
+     * @var ServerService
+     */
+    protected $server;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
     {
-        #
+        self::bootKernel();
+
+        $this->entityManager = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $this->user = new UserService($this->entityManager);
+        $this->server = new ServerService($this->entityManager);
+    }
+
+    /**
+     * Test Insert
+     *
+     * @return void
+     */
+    public function testInsert()
+    {
+        $user = $this->user->getByUsername('lwt');
+
+        $this->assertTrue($this->server->insert([
+            'asset_id' => '87523',
+            'user' => $user,
+            'name' => 'K123',
+            'brand' => 'Dell',
+            'price' => '300.03',
+        ]));
+        $this->assertTrue($this->server->insert([
+            'asset_id' => '87864',
+            'user' => $user,
+            'name' => 'K183',
+            'brand' => 'HP',
+            'price' => '890',
+        ]));
+    }
+
+
+    /**
+     * Test Get By Asset Id
+     *
+     * @return void
+     */
+    public function testGetByAssetId()
+    {
+        $user = $this->user->getByUsername('lwt');
+        $server = $this->server->getByAssetId('87523');
+        $this->assertEquals('K123', $server->getName());
+    }
+
+    /**
+     * Test Get By Id
+     *
+     * @return void
+     */
+    public function testGetById()
+    {
+        $user = $this->user->getByUsername('lwt');
+        $server = $this->server->getByAssetId('87523');
+        $server = $this->server->getById($server->getId());
+        $this->assertEquals('K123', $server->getName());
+    }
+
+    /**
+     * Test Check Asset Id
+     *
+     * @return void
+     */
+    public function testCheckAssetId()
+    {
+        $this->assertTrue($this->server->checkAssetId('87523'));
+    }
+
+    /**
+     * Test Update By Id
+     *
+     * @return void
+     */
+    public function testUpdateById()
+    {
+        $server = $this->server->getByAssetId('87523');
+        $this->assertTrue($this->server->updateById($server->getId(), [
+            'name' => 'P273',
+            'brand' => 'Apple'
+        ]));
+    }
+
+    /**
+     * Test Update By Asset Id
+     *
+     * @return void
+     */
+    public function testUpdateByAssetId()
+    {
+        $this->assertTrue($this->server->updateByAssetId('87864', [
+            'name' => 'O2662',
+            'brand' => 'Samsung'
+        ]));
+    }
+
+    /**
+     * Test Delete By Id
+     *
+     * @return void
+     */
+    public function testDeleteById()
+    {
+        $server = $this->server->getByAssetId('87523');
+        $this->assertTrue($this->server->deleteById($server->getId()));
+    }
+
+    /**
+     * Test Delete By Asset Id
+     *
+     * @return void
+     */
+    public function testDeleteByAssetId()
+    {
+        $this->assertTrue($this->server->deleteByAssetId('87864'));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
     }
 }
